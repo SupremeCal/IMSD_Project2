aace121bc78a58853526bf27f82b40becab0e40e
Patrick Rufino <patrick.rufino@gmail.com>
2019-10-30 10:27:54 -0400

    Prototype

---
+++ Assets/WaterGoUp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterGoUp : MonoBehaviour
+{
+    float WaterYPos = 0.1f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale += new Vector3(0, WaterYPos * Time.deltaTime, 0);
+
+    }
+}
