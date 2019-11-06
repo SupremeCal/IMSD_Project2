902564d5f62d8104122d23c69a96b7b471f5fffc
Patrick Rufino <patrick.rufino@gmail.com>
2019-11-05 22:07:53 -0500

    Update 5
    Added timeline, female, kid, day and night cycle, fixed the water' timers

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    private float Mantimer;
+
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        Mantimer += Time.deltaTime;
+
+        //transform.position += new Vector3(1 * Time.deltaTime, 0 , 0);
+
+        if (Mantimer > 0 && Mantimer < 10)
+        {
+            transform.position += new Vector3(0.36f * Time.deltaTime, 0, 0);
+            
+        }
+
+        if (Mantimer > 20 && Mantimer < 30)
+        {
+            transform.position += new Vector3(0.36f * Time.deltaTime, 0, 0);
+
+        }
+
+        if (Mantimer > 40 && Mantimer < 50)
+        {
+            transform.position += new Vector3(0.36f * Time.deltaTime, 0, 0);
+
+        }
+    }
+}
--- Assets/Scripts/WaterGoUp.cs
+++ Assets/Scripts/WaterGoUp.cs
@@ -6,10 +6,12 @@ public class WaterGoUp : MonoBehaviour
 {
     float WaterYScale = 0.1f;
     private float timer;
+    public Light DayandNight;
+    float r;
     
     void Start()
     {
-        
+        DayandNight.color = Color.white;
     }
 
     void Update()
@@ -17,25 +19,31 @@ public class WaterGoUp : MonoBehaviour
         //transform.localScale += new Vector3(0, WaterYScale * Time.deltaTime, 0);
 
         timer += Time.deltaTime;
+        //Day and Night color change
+        r = (Mathf.Sin(timer * 3) / 2) + .5f;
 
         if (timer > 0 && timer < 10)
         {
             transform.localScale += new Vector3(0, WaterYScale * Time.deltaTime, 0);
+            DayandNight.color = new Color(r,r,r);
         }
 
         if(timer > 20 && timer < 30)
         {
             transform.localScale += new Vector3(0, WaterYScale * Time.deltaTime, 0);
+            DayandNight.color = new Color(r, r, r);
         }
 
         if (timer > 40 && timer < 50)
         {
             transform.localScale += new Vector3(0, WaterYScale * Time.deltaTime, 0);
+            DayandNight.color = new Color(r, r, r);
         }
 
         if (timer > 60 && timer < 70)
         {
             transform.localScale += new Vector3(0, WaterYScale * Time.deltaTime, 0);
+            DayandNight.color = new Color(r, r, r);
         }
 
     }
